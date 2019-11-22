bf26104fe12c144d5e7174416fe394356baf358c
Craig Donavin <crdonavin@gmail.com>
2019-11-22 02:59:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const farmAnimals = 'cow horse sheep pig chicken'
 
-const [moo, neigh, baa] = farmAnimals.split(' ')
+const [moo, neigh, baa, oink] = farmAnimals.split(' ')
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
